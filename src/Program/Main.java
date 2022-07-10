26ae2947840f27c4366538f53630d9a570e82f22
LucasPBernard <lucas.p.bernard@gmail.com>
2022-07-09 19:53:23 -0500

    first commit

---
+++ src/Program/Main.java
@@ -0,0 +1,9 @@
+package Program;
+import static Program.WordCounter.wordCounter;
+
+public class Main {
+    public static void main(String[] args) {
+        String someWords = "here are some other words. and even more words.";
+        wordCounter(someWords);
+    }
+}
---
+++ src/Program/WordCounter.java
@@ -0,0 +1,19 @@
+package Program;
+
+import java.util.Arrays;
+
+public class WordCounter {
+    public static int wordCounter(String words){
+        String noPunct = words.replaceAll("\\p{Punct}", "");
+        String[] wordsSplit = noPunct.split(" ");
+        int wordNumber= 0;
+        for (int i=0; i<wordsSplit.length;i++) {
+            wordNumber++;
+            //System.out.println(wordsSplit[i]);
+        }
+        //System.out.println(Arrays.toString(wordsSplit));
+        System.out.println(wordNumber);
+        return wordNumber;
+    }
+
+}
