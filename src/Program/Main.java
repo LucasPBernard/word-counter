a581edc487deed7869ddeaa108e84494c9b109ce
LucasPBernard <lucas.p.bernard@gmail.com>
2022-07-10 02:28:19 -0500

    user input

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -2,6 +2,7 @@ package Program;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 import static Program.WordCounter.wordCounter;
 
@@ -12,7 +13,19 @@ public class Main {
         String someWordsFile = Files.readString(filePath);
         String someWordsResult = "There are " + wordCounter(someWords) + " words in the string someWords";
         String someWordsFileResult = "There are " + wordCounter(someWordsFile) + " words in the file someWords.txt";
+
+        System.out.println("I can count the words in the string: someWords, \nin the file: someWords.txt," +
+                " \nand in the words you type.");
+        System.out.println();
         System.out.println(someWordsResult);
         System.out.println(someWordsFileResult);
+
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.println("Type some words and I will count them.");
+        String someWordsTyped = input.nextLine();
+        String someWordsTypedResult = "You just typed " + wordCounter(someWordsTyped) + " word[s].";
+        System.out.println(someWordsTypedResult);
+        input.close();
     }
 }
--- src/Program/WordCounter.java
+++ src/Program/WordCounter.java
@@ -4,6 +4,8 @@ public class WordCounter {
     public static int wordCounter(String words){
         String noPunct = words.replaceAll("\\p{Punct}", "");
         String[] wordsSplit = noPunct.split(" ");
+        //System.out.println(words);
+
         int wordNumber= 0;
         for (int i=0; i<wordsSplit.length;i++) {
             wordNumber++;
