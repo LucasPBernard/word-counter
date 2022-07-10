d3a0d37be9f117f7fd467765d60fbf14934a8df4
LucasPBernard <lucas.p.bernard@gmail.com>
2022-07-09 23:15:07 -0500

    txt file word counter

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -8,7 +8,7 @@ import static Program.WordCounter.wordCounter;
 public class Main {
     public static void main(String[] args) throws IOException {
         String someWords = "here are some other words. and even more words.";
-        Path filePath = Path.of("d:/school/repository/word-counter/someWords.txt");
+        Path filePath = Path.of("../word-counter/someWords.txt");
         String someWordsFile = Files.readString(filePath);
         String someWordsResult = "There are " + wordCounter(someWords) + " words in String someWords";
         String someWordsFileResult = "There are " + wordCounter(someWordsFile) + " words in File someWords.txt";
